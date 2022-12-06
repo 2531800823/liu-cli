792fc7e0059b0c2939a569bf09dafd7e506eb532
刘士朋 <2531800823@qq.con>
2022-12-06 22:31:30 +0800

    feat: 完成 clear 命令

--- bin/index.js
+++ bin/index.js
@@ -5,37 +5,26 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const pkg = require('../package.json');
 
-program.on("--help", function() {
+const { clear } = require('../lib');
+
+program.on('--help', function() {
   console.log(
-    "\r\n" +
-      figlet.textSync("liu-cli", {
-        font: "3D-ASCII",
-        horizontalLayout: "default",
-        verticalLayout: "default",
+    '\r\n' +
+      figlet.textSync('liu-cli', {
+        font: '3D-ASCII',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
         width: 80,
         whitespaceBreak: true,
-      })
+      }),
   );
   // 前后两个空行调整格式，更舒适
-  console.log(
-    `version: ${chalk.cyan(
-      pkg.version
-    )}`
-  );
+  console.log(`version: ${chalk.cyan(pkg.version)}`);
   console.log();
-  console.log(
-    `Run ${chalk.cyan(
-      "liu-cli <command> --help"
-    )} for detailed usage of given command.`
-  );
+  console.log(`Run ${chalk.cyan('liu-cli <command> --help')} for detailed usage of given command.`);
   console.log();
-  console.log(
-    `github: ${chalk.cyan(
-      "https://github.com/2531800823"
-    )}`
-  );
+  console.log(`github: ${chalk.cyan('https://github.com/2531800823')}`);
 });
-
 
 program
   .command('create <project-name>') // 增加创建指令
@@ -43,15 +32,18 @@ program
   .option('-f, --force', 'overwrite target directory if it exists') // 强制覆盖
   .action((projectName, cmd) => {
     // 处理用户输入create 指令附加的参数
-    console.log(projectName,cmd);
+    console.log(projectName, cmd);
   });
 
 program
-  .name("liu-cli")
+  .command('clear') // 增加创建指令
+  .description('删除 node_modules ') // 添加描述信息
+  .option('-d, --dir <dir_name>', '删除指定目录下的') // 强制覆盖
+  .action(clear);
+
+program
+  .name('liu-cli')
   .usage(`<command> [option]`)
-  .version( `version: ${chalk.cyan(
-    pkg.version
-  )}`);
+  .version(`version: ${chalk.cyan(pkg.version)}`);
 
 program.parse(process.argv);
-
